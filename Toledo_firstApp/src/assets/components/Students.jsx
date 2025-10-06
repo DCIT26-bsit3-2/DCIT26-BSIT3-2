2d49ba75daf1c21fa59648c2fd78698e30355835
Marc David Toledo <marcdavid.toledo@cvsu.edu.ph>
2025-10-06 16:46:58 +0800

    commit

--- Toledo_firstApp/src/assets/components/Students.jsx
+++ Toledo_firstApp/src/assets/components/Students.jsx
@@ -5,20 +5,17 @@ import React from 'react';
 function Students(student) {
     return (
     <>
-    <div>
-        
-                <p>Student Name: {student.name}</p>
-                <p>Student Number: {student.number}</p>
-                <p> Age: {student.age}</p>
-                <p> Course: {student.course}</p>
-                <p> Year Level: {student.year}</p>
-                <hr />
-            
-        
+    <div>       
+    <p>Student Name: {student.name}</p>
+    <p>Student Number: {student.number}</p>
+    <p> Age: {student.age}</p>
+    <p> Course: {student.course}</p>
+    <p> Year Level: {student.year}</p>
+    <hr />    
     </div>
     
     </>
     )
 }
 
-export default Header;
+export default Students;HH
