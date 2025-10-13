9122c03c93ac61c276d6bb38e15d0ae557e4f4c1
JannahLero <jannahmae.lero@cvsu.edu.ph>
2025-10-13 17:10:07 +0800

    lab3

---
+++ quilon_firstApp/src/assets/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer className="bg-gray-800 text-white text-center p-4 shadow-inner fixed bottom-0 left-0 w-full">
+            Website Copyrighted by Jommel Quilon
+        </footer>
+    );
+}
+
+export default Footer;
